dbb38ad3b7e5050e8e8f0545a32fb9c9f0ab5683
Paulo Humberto <phumbertodasilva@gmail.com>
2020-07-28 23:57:05 -0300

    - Removendo type object

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -16,7 +16,7 @@ import { Container, Content, Background } from './styles';
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const handleSubmit = useCallback(async (data: Record<string, unknown>) => {
     try {
       formRef.current?.setErrors({});
 
